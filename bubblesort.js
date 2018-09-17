9dc47bcec1e43f30fd9932174d529de8b9055772
Hannah Barton <hannbarton@gmail.com>
2018-09-17 12:31:40 -0400

    working files

---
+++ bubblesort.js
@@ -0,0 +1,20 @@
+function bubbleSortNum(array) {
+  return 'cat';
+}
+
+function swap(a, b) {
+    if (a < b) {
+        return a
+    }
+    else {
+        compare(arr[0] arr[1])
+    }
+}
+
+function compare(a, b) {
+    if (a < b) {
+        return false
+    } else {
+        return true;
+    }
+}
---
+++ bubblesort.spec.js
@@ -0,0 +1,22 @@
+describe('a bubble sort by number function', function(){
+    it('handles one item in an array', function(){
+      expect( bubbleSortNum([1]) ).toEqual( [1] );
+    });
+
+    it('returns smallest to largest', function(){
+      expect( bubbleSort([4,3,2,1]) ).toEqual( [1,2,3,4] );
+    });
+
+    //we want to have 2 for-loops that compare smallest to largest
+    //twoSwapFunc = [1,2];
+    //helperfunction = return true or false;
+    //swapper = does the swapping
+    beforeAll(function () {
+        spyOn(window, 'swap').and.callThrough();
+    });
+      it('called the helperfunction called swap', function () {
+        bubbleSortNum.swap();
+        expect(bubbleSortNum.swap.calls.count([6,5,4])).toEqual(3);
+    });
+
+});
